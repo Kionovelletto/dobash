b9df68442d20762065b72680cabaa610be269562
Caio H. Novelletto <kionovelletto@gmail.com>
2023-01-10 15:35:37 -0300

    Create EmojiBash.sh

---
+++ EmojiBash.sh
@@ -0,0 +1,39 @@
+Emoji para Bash
+
+#Coloque emoji em seu shell script
+
+printf '\342\232\231\n' = Configuração ⚙
+
+printf '\360\237\220\247' = pinguim 🐧
+
+printf '\360\237\227\221\n' = Lixeira 🗑
+
+printf '\342\254\207\n' =  Seta para baixo ⬇
+
+printf '\342\217\261\n' = Cronômetro ⏱
+
+printf '\360\237\222\276\n' = Disquete 💾
+
+printf '\360\237\223\212\n' =  Gráficos 📊
+
+printf '\360\237\223\264\n' =  Telefone off 📴
+
+printf '\360\237\220\215\n' = Cobra 🐍
+
+printf '\360\237\220\263\n' = Baleia/Docker 🐳
+
+printf '\360\237\232\254\n' = Cigarro 🚬
+
+printf '\360\237\220\261\n' = Gato 🐱
+
+printf '\360\237\221\223\n' = Óculos de sol 👓
+
+printf '\360\237\222\273\n' = Notebook 💻
+
+printf '\360\237\232\252\n' = Porta 🚪
+
+printf '\360\237\223\213\n' = Prancheta/Documento 📋
+
+printf '\360\237\223\244\n' = Compactação/zip 📤
+
+printf '\360\237\222\243\n' = Bomba 💣
