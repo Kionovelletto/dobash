7ac1382471f39d7c41b08c0d9defb0ae5e364e10
Caio H. Novelletto <kionovelletto@gmail.com>
2023-02-17 17:44:51 -0300

    Update animation02_progress.sh

--- animation_shell/animation02_progress.sh
+++ animation_shell/animation02_progress.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-while true; do echo -n .; sleep 1; done &
-echo -e "I want coffee! \n Please Wait Running: "
-sleep 10
-echo done
+while true; 
+  do echo -n .; sleep 1; done &
+  echo -e "I want coffee! \n Please Wait Running: "
+  sleep 10
+  echo done
 killall animation02_progress.sh
